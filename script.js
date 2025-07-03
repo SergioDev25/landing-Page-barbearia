659bdf6c12c4ebf10f6e9404c847c1d6ea5fec82
Sérgio Antônio <antonio24422019@gmail.com>
2025-07-03 17:59:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  const menuToggle = document.getElementById('menu-toggle');
+  const navbar = document.getElementById('navbar');
+
+  menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+  });
+
